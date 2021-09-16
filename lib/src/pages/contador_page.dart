f046cc4f1d0b7fa9c31545fb76ab4c26848ef592
Edd-war <digiespirit.1807@gmail.com>
2021-09-16 12:19:53 -0500

    Creando más acciones en nuestro contador
    Se apartó la instrucción de la creación del botón acumulador con una nueva función y se implementaron otros dos botones ordenadamente separados pero sin funcionar aún.

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -31,16 +31,24 @@ class _ContadorPageState extends State<ContadorPage>{
         ),
       ),
       //floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          //print('Hola Mundo');
-
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
+      floatingActionButton: _crearBotones()
     );
   }
+
+  Widget _crearBotones(){
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: null),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: null),
+        SizedBox(width: 5.0,),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: null),
+      ],
+    );
+
+  }
+  
 }
