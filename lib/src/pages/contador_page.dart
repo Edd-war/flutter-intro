043d4e5dbc556a8f34e78741f9b467da6ce55148
Edd-war <digiespirit.1807@gmail.com>
2021-09-16 11:56:22 -0500

    StatefullWidget
    Se llegó a la plantilla por defecto cuando se crea una app Flutter.

---
+++ lib/src/pages/contador_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+class ContadorPage extends StatefulWidget{
+
+  @override
+  createState() => _ContadorPageState();
+
+}
+
+class _ContadorPageState extends State<ContadorPage>{
+
+  final _estiloTexto = new TextStyle(fontSize: 25);
+
+  int _conteo = 0;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de taps', style: _estiloTexto),
+            Text('$_conteo', style: _estiloTexto),
+          ],
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          //print('Hola Mundo');
+
+          setState(() {
+            _conteo++;
+          });
+        },
+      ),
+    );
+  }
+}
